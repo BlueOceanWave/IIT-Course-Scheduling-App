f86641855181d4345831c8868039f0df86573772
LMarcin12 <mlanda2@hawk.iit.edu>
2023-09-26 21:53:28 -0500

    Scrapes Evrey Class into a list

--- classesScraper.py
+++ classesScraper.py
@@ -3,7 +3,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-#path = "C:/Users/marci/Desktop/School/chromedriver.exe"
+class_list = [] 
+def choose_subject(i):
+    # Second PAGE #
+    #Choose a Subject
+    selectSubject = Select(driver.find_elements(By.NAME, 'sel_subj')[1])
+    selectSubject.deselect_all()
+    selectSubject.select_by_index(i)
+
+    #Click Submit
+    classSearch = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
+    classSearch.click()
+
+def get_data():
+    # THIRD PAGE #
+#Get all of the Sections
+    ddTitle = driver.find_elements(By.CLASS_NAME, 'ddtitle')
+    ddDefault = driver.find_elements(By.CLASS_NAME, 'dddefault')
+
+    
+    for p in range(len(ddTitle)):
+        list = ddTitle[p].text.split(" - ") # Title, CRN, (sID, cID), sNum
+        info = ddDefault[0].text.split("\n")
+        list.append(info[1].split(" ")[2] + " " + info[1].split(" ")[3]) #term
+        list.append(info[6].split(" ")[0]) #campus
+        list.append(info[8].split(" ")[0]) #online
+        for i in range (2, 8):
+            #startTime, endTime, days, building, room, startDate, endDate, cType, instructor(s)
+            if(i == 2 or i == 5):
+                times = ddDefault[i + (p * 8)].text.split(" - ")
+                s = "".join(times[0:-1])
+                list.append(s) 
+                list.append(times[-1]) 
+            elif(i == 4):
+                loc = ddDefault[i + (p * 8)].text.split(" ")
+                list.append(" ".join(loc[0:-1])) 
+                if(loc[0] != "TBA"):
+                    list.append(loc[-1])   
+            else:
+                list.append(ddDefault[i + (p * 8)].text) 
+        if(p == 0):
+            print(list)
+        class_list.append(list)
+    returnToPrev = driver.find_element(By.XPATH, '/html/body/div[3]/table[2]/tbody/tr/td/a')
+    returnToPrev.click()
+
+    #time.sleep(1)   
 
 
 driver = webdriver.Chrome()
@@ -17,28 +62,8 @@ searchForTerm.select_by_index(1)
 submit = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
 submit.click()
 
-    # Second PAGE #
-#Choose a Subject
-selectSubject = Select(driver.find_elements(By.NAME, 'sel_subj')[1])
-selectSubject.select_by_index(11)
-
-#Click Submit
-classSearch = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
-classSearch.click()
-
-    # THIRD PAGE #
-#Get all of the Sectionm Titles
-ddTitle = driver.find_elements(By.CLASS_NAME, 'ddtitle')
-
-class_list = []
-for p in range(len(ddTitle)):
-    list = ddTitle[p].text.split(" - ") # Title, CRN, (sID, cID), sNum
-    if(p < 10):
-        print(list)
-    class_list.append(ddTitle[p].text)
-
-time.sleep(10)
-'''
-
-'''
-driver.close
+select = driver.find_elements(By.NAME, 'sel_subj')[1]
+for i in range (len(select.find_elements(By.CSS_SELECTOR, '*'))):
+    print(i)
+    choose_subject(i)
+    get_data()
