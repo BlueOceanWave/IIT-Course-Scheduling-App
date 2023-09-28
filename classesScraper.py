bbf1d5ad52bfb5da469a03434838edfacfad9660
LMarcin12 <mlanda2@hawk.iit.edu>
2023-09-28 14:49:47 -0500

    Finished Scraper and JSON file

--- classesScraper.py
+++ classesScraper.py
@@ -1,4 +1,5 @@
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -24,8 +25,10 @@ def get_data():
     
     for p in range(len(ddTitle)):
         list = ddTitle[p].text.split(" - ") # Title, CRN, (sID, cID), sNum
+        if(len(list) == 5):
+            list.pop(0)
         info = ddDefault[0].text.split("\n")
-        #print(info)
+        
         infoEx = info[1].split(" ")[2]
         if(infoEx == "Fall" or infoEx == "Spring" or infoEx == "Summer"):
             n = 1
@@ -48,17 +51,44 @@ def get_data():
                     list.append(loc[-1])   
             else:
                 list.append(ddDefault[i + (p * 8)].text) 
-        #if(p == 0):
-        #    print(list)
-        class_list.append(list)
+
+        IDSplit = list[2].split(" ")
+        print(IDSplit)
+        if(len(list) == 16):
+            daysEmpty = 1
+        else:
+            daysEmpty = 0
+
+        jsonStr = { 
+            "title": list[0],
+            "CRN": list[1],
+            "sID":IDSplit[0],
+            "cID": IDSplit[1],
+            "sNum": list[3],
+            "term": list[4],
+            "campus": list[5],
+            "online": list[6],
+            "startTime": list[7],
+            "endTime": list[8],
+            "days": list[9],
+            "building": list[9 + daysEmpty],
+            "room": list[10 + daysEmpty],
+            "startDate": list[11 + daysEmpty],
+            "endDate": list[12 + daysEmpty],
+            "cType": list[13 + daysEmpty],
+            "instructors": list[14 + daysEmpty].split(", ")
+        }
+            
+        class_list.append(jsonStr)
     returnToPrev = driver.find_element(By.XPATH, '/html/body/div[3]/table[2]/tbody/tr/td/a')
     returnToPrev.click()
 
     #time.sleep(1)   
 
-
+#Open Chrome
 driver = webdriver.Chrome()
-driver.get("https://ssb.iit.edu/bnrprd/bwckschd.p_disp_dyn_sched")
+driver.get("https://ssb.iit.edu/bnrprd/bwckschd.p_disp_dyn_sched") 
+
     # FIRST PAGE #
 #Select the term of classes
 searchForTerm = Select(driver.find_element(By.NAME, 'p_term'))
@@ -68,12 +98,22 @@ searchForTerm.select_by_index(1)
 submit = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
 submit.click()
 
+#Find the dropdown
 select = driver.find_elements(By.NAME, 'sel_subj')[1]
+
+#Look through all of the elements in the dropdown (every subject)
 for i in range (len(select.find_elements(By.CSS_SELECTOR, '*'))):
-    print(i)
+    #choose the subject at index i and open that webpage
     choose_subject(i)
+    #get each of the classes from that webpage into seperate JSON strings
+    #append each class to the whole class list
+    #return to previous webpage
     get_data()
 
+#create a .json file from all of the classes
+outputFileName = "Fall_2023.json"
+with open(outputFileName, 'w') as json_file:  
+    json_string = json.dump(class_list, json_file, indent = 4)
 
 '''
 JSON:
