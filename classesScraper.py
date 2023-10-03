1e51f2ff4a21f108fafc0fce18a8dea34bf28d62
LMarcin12 <mlanda2@hawk.iit.edu>
2023-10-03 00:00:54 -0500

    Fixed the Scrapper and the JSON

--- classesScraper.py
+++ classesScraper.py
@@ -26,52 +26,74 @@ def get_data():
 # THIRD PAGE #
     #Get all of the Sections
     ddTitle = driver.find_elements(By.CLASS_NAME, 'ddtitle')
+    mainGroup = driver.find_element(By.XPATH, "/html/body/div[3]/table[1]/tbody")
+    children = mainGroup.find_elements(By.CSS_SELECTOR, "div > table > tbody > tr > td.dddefault")
     ddDefault = driver.find_elements(By.CLASS_NAME, 'dddefault')
-
     
     for p in range(len(ddTitle)):
         list = ddTitle[p].text.split(" - ") # title, CRN, (sID, cID), sNum
-
+        
         #Sometimes theres an extra first element, if so, remove it
         if(len(list) == 5):
             list.pop(0)
-        info = ddDefault[0].text.split("\n")
+        
+        info = children[p].text.split("\n")
+        
+        if(info[0][0:15] != "Associated Term"):
+            info.pop(0)
+        if(info[5].split(" ")[-1] != "Campus"):
+            info.insert(3, "Filler Line")
 
+#ERRORS
+        if(info[0][0:15] != "Associated Term"):
+            print("\nERROR A: " + list[1] + ", " + list[2] + ", " +  info[0])
+
+        if(info[5].split(" ")[-1] != "Campus"):
+            print("\nERROR B: " + list[1] + ", " + list[2] + ", " +  info[5])
+
+        if(info[7].split(" ")[-1] != "Method"):
+            print("\nERROR C: " + list[1] + ", " + list[2] + ", " +  info[7])
+#ERRORS
         #Gets the semester ("Spring", "Summer" or "Fall")
-        sem = info[1].split(" ")[2] 
-        if(sem == "Fall" or sem == "Spring" or sem == "Summer"):
-            n = 1
-        else: 
-            n = 0
+        sem = info[0].split(" ")[2] 
 
-        list.append(info[n].split(" ")[2] + " " + info[n].split(" ")[3]) #term
-        list.append(info[n + 5].split(" ")[0]) #campus
-        list.append(info[n + 7].split(" ")[0]) #online
+        list.append(info[0].split(" ")[2] + " " + info[0].split(" ")[3]) #term
+        list.append(info[5].split(" ")[0]) #campus
+         #online  -- Needs to no longer be boolean (Non Traditional, Online, Traditional)
+        if(info[7].split(" ")[0] == "Non"):
+            list.append(info[7].split(" ")[0] + " " + info[7].split(" ")[1])
+        else:
+            list.append(info[7].split(" ")[0])
+            
+        table = children[p].find_elements(By.CSS_SELECTOR, "td.dddefault")
 
-        for i in range (2, 8):
+        for i in range (1, 7):
+            #(startTime, endTime), days, (building, room), (startDate, endDate), cType, instructors
             #startTime, endTime, days, building, room, startDate, endDate, cType, instructor(s)
-            if(i == 2 or i == 5):
-                times = ddDefault[i + (p * 8)].text.split(" - ")
-                s = "".join(times[0:-1])
-                list.append(s) #startTime/startDate
-                list.append(times[-1]) #endTime/endDate
-            elif(i == 4):
-                loc = ddDefault[i + (p * 8)].text.split(" ")
-                list.append(" ".join(loc[0:-1])) 
-                if(loc[0] != "TBA"):
+
+            if(i == 1 or i == 4):
+                if(table[i].text == "TBA"):
+                    list.append("TBA")
+                    list.append("TBA")
+                else:
+                    times = table[i].text.split(" - ")
+                    s = "".join(times[0:-1])
+                    list.append(s) #startTime/startDate
+                    list.append(times[-1]) #endTime/endDate
+            elif(i == 3):
+                if(table[i].text == "TBA"):
+                    list.append("TBA")
+                    list.append("TBA")
+                else:
+                    loc = table[i].text.split(" ")
+                    list.append(" ".join(loc[0:-1])) 
                     list.append(loc[-1])   
             else:
-                list.append(ddDefault[i + (p * 8)].text) 
+                list.append(table[i].text) 
 
         #Splits sID and cID
         IDSplit = list[2].split(" ")
-       
-       #Sometimes days is empty, if it is, subtract 1 from the index of everything after
-        if(len(list) == 16):
-            daysEmpty = 1
-        else:
-            daysEmpty = 0
-
+        
         jsonStr = { 
             "title": list[0],
             "CRN": list[1],
@@ -84,19 +106,25 @@ def get_data():
             "startTime": list[7],
             "endTime": list[8],
             "days": list[9],
-            "building": list[9 + daysEmpty],
-            "room": list[10 + daysEmpty],
-            "startDate": list[11 + daysEmpty],
-            "endDate": list[12 + daysEmpty],
-            "cType": list[13 + daysEmpty],
-            "instructors": list[14 + daysEmpty].split(", ")
+            "building": list[10],
+            "room": list[11],
+            "startDate": list[12],
+            "endDate": list[13],
+            "cType": list[14],
+            "instructors": list[15].split(", ")
         }
-            
+
         class_list.append(jsonStr)
     returnToPrev = driver.find_element(By.XPATH, '/html/body/div[3]/table[2]/tbody/tr/td/a')
     returnToPrev.click()
 
     #time.sleep(1)   
+
+def to_JSON():
+    #create a .json file from all of the classes
+    outputFileName = "Fall_2023.json"
+    with open(outputFileName, 'w') as json_file:  
+        json_string = json.dump(class_list, json_file, indent = 4)
 
 #Open Chrome
 driver = webdriver.Chrome()
@@ -105,7 +133,7 @@ driver.get("https://ssb.iit.edu/bnrprd/bwckschd.p_disp_dyn_sched")
     # FIRST PAGE #
 #Select the term of classes
 searchForTerm = Select(driver.find_element(By.NAME, 'p_term'))
-searchForTerm.select_by_index(1)
+searchForTerm.select_by_index(1) #First Term In the Dropdown
 
 #Click Submit
 submit = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
@@ -116,14 +144,10 @@ select = driver.find_elements(By.NAME, 'sel_subj')[1]
 
 #Look through all of the elements in the dropdown (every subject)
 for i in range (len(select.find_elements(By.CSS_SELECTOR, '*'))):
-    
     choose_subject(i)
     get_data()
 
-#create a .json file from all of the classes
-outputFileName = "Fall_2023.json"
-with open(outputFileName, 'w') as json_file:  
-    json_string = json.dump(class_list, json_file, indent = 4)
+to_JSON()
 
 '''
 JSON:
