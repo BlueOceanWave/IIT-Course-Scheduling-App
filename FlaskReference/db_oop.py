23264a6eefafdbcd074c1fd15cc2f602730ab5ec
RobertJohnSoler <rsoler@hawk.iit.edu>
2023-10-15 20:35:42 -0500

    can now fetch json from database

--- FlaskReference/db_oop.py
+++ FlaskReference/db_oop.py
@@ -1,4 +1,6 @@
 import psycopg2
+import json
+from datetime import time
 
 # This is my attempt at making an object-oriented approach at communicating with the database
 # The goal is to make a user object, and link it to the database.
@@ -75,3 +77,59 @@ class student_account:
             print("Major not in database")
             return
 
+
+class classes:
+    table = "classes"
+    crn = ""
+    sid = ""
+    cid = ""
+    snum = ""
+    term = ""
+    days = ""
+    starttime = ""
+    endtime =""
+    campus = ""
+    online = ""
+    building = ""
+    room = ""
+
+    def __init__(self, crn="", sid="", cid="", snum="", term="", days="", starttime="", endtime="", campus="", online="", building="", room=""):
+        self.crn = crn
+        self.sid = sid
+        self.cid = cid
+        self.snum = snum
+        self.term = term
+        self.days = days
+        self.starttime = starttime
+        self.endtime = endtime
+        self.campus = campus
+        self.online = online
+        self.building = building
+        self.room = room
+
+def getAllClasses():
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM classes;")
+        rows = cursor.fetchall()  # fetch all the rows from the table
+        # Get the column names for dictionary keys
+        column_names = [desc[0] for desc in cursor.description]
+        data = []
+        for row in rows:
+            row_dict = dict(zip(column_names, row))  # turn each row into a dictionary
+            # Convert time objects to string representations
+            if row_dict['starttime'] is not None:
+                row_dict['starttime'] = row_dict['starttime'].strftime('%H:%M:%S')
+            if row_dict['endtime'] is not None:
+                row_dict['endtime'] = row_dict['endtime'].strftime('%H:%M:%S')
+            data.append(row_dict)  # append them to the data list
+        # Serialize the data to JSON
+        json_data = json.dumps(data, default=str)  # Use default=str to serialize other non-serializable objects as strings
+        return json_data
+    except psycopg2.Error as e:
+        # Handle database connection or query errors here
+        print("Error:", e)
+        return None
+    finally:
+        if connection:
+            connection.close()
--- FlaskReference/website.py
+++ FlaskReference/website.py
@@ -3,10 +3,10 @@ from jinja2 import Environment
 from flask_socketio import SocketIO
 from cryptography.fernet import Fernet
 import db
-from db_oop import student_account
+from db_oop import student_account, classes, getAllClasses
 
 '''This is where flask application is being made.'''
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 key = b'-NwUP8bcuumHeCWIJPz2L_MQimrSxUCKXZaNwYHRbQU='
 cipher = Fernet(key)
@@ -28,35 +28,15 @@ def major():
 def login():
     return render_template("login.html")    # renders and executes index.html
 
-# This version of the /result route uses the db.py layout. 
-# The other version follows the db_test.py layout. They are different.
-# @app.route("/result", methods = ['POST', 'GET'])
-# def result():
-#     source = request.form.get('source')
+@app.route("/courses", methods=['GET'])
+def get_courses():
+    classJSON = getAllClasses()
+    # print(classJSON)
+    return classJSON
 
-#     if source == "signup":
-#         name_input = request.form.get("name")          # Extracts the "name" field from the form
-#         print("name is", name_input)
-#         password_input = request.form.get("pass")  # Extracts the "name" field from the dictionary
-#         major_input = request.form.get("major")
-#         db.newEntry(name_input, password_input, major_input)
-#         return render_template("signup.html", done=True)
-
-#     elif source == "login":
-#         name_input = request.form.get("name")   # Extracts the "name" field from the form
-#         password_input = request.form.get("pass")  # Extracts the "name" field from the form
-#         print("name is ", name_input)
-#         if db.searchEntry(name_input) is False:
-#             return "Invalid credentials!"
-#         else:
-#             db_pass = db.getEntry(name_input)[1]
-#             print("password is", db_pass)
-#             if db_pass != password_input:
-#                 return "Invalid credentials!!"
-#             else:
-#                 db_major = db.getEntry(name_input)[2]
-#                 return Markup(render_template("welcome.html", name=name_input, major=db_major))  # renders and executes index.html again,
-#                                                            # but this time with the given name
+@app.route("/course_display", methods=['GET'])
+def show_classes():
+    return render_template("course_display.html")    
 
 
 # This version of the /result route uses the object-oriented approach for the database entries (see db_test.py)
