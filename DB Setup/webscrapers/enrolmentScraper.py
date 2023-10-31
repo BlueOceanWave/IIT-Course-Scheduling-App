ec7d86e691f85c750c5adfcb63b7ced567a0651e
LMarcin12 <mlanda2@hawk.iit.edu>
2023-10-30 21:28:17 -0500

    Scrapped all enrollment info for Fall 2023

--- DB Setup/webscrapers/enrolmentScraper.py
+++ DB Setup/webscrapers/enrolmentScraper.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 # List of all classes, each in JSON format
-class_list = [] 
+class_list = {}
 
 # Choose the subject at index i and open that webpage
 def choose_subject(i):
@@ -29,7 +29,7 @@ def get_data(CRN):
     ddDefault = driver.find_elements(By.CLASS_NAME, 'dddefault')
     
     c = driver.find_element(By.XPATH, '/html/body/div[3]/table[1]/tbody/tr[1]/th')
-    print(c.text)
+    #print(c.text)
     
 
     for p in range(len(ddTitle)):
@@ -38,8 +38,8 @@ def get_data(CRN):
         if(len(list) == 5):
             list.pop(0)
         if(list[1] == CRN):
-            print(list)
-            print(ddTitle[p].text)
+            #print(list)
+            #print(ddTitle[p].text)
             ddTitle[p].find_element(By.CSS_SELECTOR, 'a').click()
             table = driver.find_element(By.XPATH, '/html/body/div[3]/table[1]/tbody/tr[2]/td/table/tbody')
             seats = table.find_elements(By.CSS_SELECTOR, 'tr')[1].find_elements(By.CLASS_NAME, "dddefault")
@@ -59,6 +59,48 @@ def get_data(CRN):
         
     # If we get here that means no class was found
     return None
+
+def get_all_data():
+# THIRD PAGE #
+    #Get all of the Sections
+    ddTitle = driver.find_elements(By.CLASS_NAME, 'ddtitle')
+    mainGroup = driver.find_element(By.XPATH, "/html/body/div[3]/table[1]/tbody")
+    children = mainGroup.find_elements(By.CSS_SELECTOR, "div > table > tbody > tr > td.dddefault")
+    ddDefault = driver.find_elements(By.CLASS_NAME, 'dddefault')
+    
+    c = driver.find_element(By.XPATH, '/html/body/div[3]/table[1]/tbody/tr[1]/th')
+    #print(c.text)
+    
+
+    for p in range(len(ddTitle)):
+        ddTitle = driver.find_elements(By.CLASS_NAME, 'ddtitle')
+        list = ddTitle[p].text.split(" - ") # title, CRN, (sID, cID), sNum
+        
+        if(len(list) == 5):
+            list.pop(0)
+        
+        ddTitle[p].find_element(By.CSS_SELECTOR, 'a').click()
+        table = driver.find_element(By.XPATH, '/html/body/div[3]/table[1]/tbody/tr[2]/td/table/tbody')
+        seats = table.find_elements(By.CSS_SELECTOR, 'tr')[1].find_elements(By.CLASS_NAME, "dddefault")
+        waitlist = table.find_elements(By.CSS_SELECTOR, 'tr')[2].find_elements(By.CLASS_NAME, "dddefault")
+
+        seatsData = {
+            "capacity": int(seats[0].text), 
+            "actual": int(seats[1].text), 
+            "remaining":  int(seats[2].text)
+            }
+        waitlistData = {
+            "capacity": int(waitlist[0].text), 
+            "actual": int(waitlist[1].text), 
+            "remaining":  int(waitlist[2].text)
+            }
+        
+        class_list[list[1]] = {"seats": seatsData, "waitlist": waitlistData}
+        returnToPrev = driver.find_element(By.XPATH, '/html/body/div[3]/table[2]/tbody/tr/td/a')
+        returnToPrev.click()
+
+    returnToPrev = driver.find_element(By.XPATH, '/html/body/div[3]/table[2]/tbody/tr/td/a')
+    returnToPrev.click()
 
 def updateEnrollment(lID, CRN): #Subject, CRN
     #Open Chrome
@@ -84,8 +126,54 @@ def updateEnrollment(lID, CRN): #Subject, CRN
 
             if data is not None:
                 return data
+            
+def getAllEnrollement(semester):
+    #Open Chrome
+    driver.get("https://ssb.iit.edu/bnrprd/bwckschd.p_disp_dyn_sched") 
 
-driver = webdriver.Chrome()
+        # FIRST PAGE #
+    #Select the term of classes
+    searchForTerm = Select(driver.find_element(By.NAME, 'p_term'))
+    searchForTerm.select_by_index(3) #Third Term In the Dropdown
 
-updateEnrollment("Chemistry", "10037")
+    #Click Submit
+    submit = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
+    submit.click()
+
+    #Find the dropdown
+    select = driver.find_elements(By.NAME, 'sel_subj')[1]
+
+    #Look through all of the elements in the dropdown (every subject)
+    for i in range (len(select.find_elements(By.CSS_SELECTOR, 'option'))):
+        choose_subject(i)
+        get_all_data()
+
+    outputFileName = "IPRO-497-Group-D\DB Setup\data\\" + semester + "_Enrollment.json"
+    with open(outputFileName, 'w') as json_file:  
+        json_string = json.dump(class_list, json_file, indent = 4)
+        print(json_string)
+            
+#    with open("IPRO-497-Group-D\DB Setup\data\\" + semester + ".json", 'r') as file:
+#        classes = file.read()#
+#
+#    with open("IPRO-497-Group-D\DB Setup\data\subjects.json", 'r') as file:
+#        subjects = file.read()
+#
+#    classesList = json.loads(classes)
+#    subjectList = json.loads(subjects)
+#
+#    for i in range(len(classesList)):
+#        for j in range(len(subjectList)):
+#            if(subjectList[j].get('sID') == classesList[i].get('sID')):
+#                lID = subjectList[j].get('lID')
+#                break
+#        print(lID, classesList[i].get('CRN'))
+#        print(updateEnrollment(lID, classesList[i].get('CRN')))
+
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome(options=op)
+
+getAllEnrollement("Fall_2023")
+#print(updateEnrollment("Chemistry", "10037"))
 
